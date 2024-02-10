5cd4355f1706f1798037b1032b296336503ea0d9
Saurabh Chandrakant Nemade <saurabhnemade@users.noreply.github.com>
2024-02-10 14:51:17 +0100

    chore: fix for baseConfig, favicon and title

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/will-you-be-my-valentine",
   plugins: [react()],
-  build: {
-    outDir: "build",
-  },
 });
