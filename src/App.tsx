d3a0197be601006166bf476b66b6424a714cc846
caty2103 <neaga.catrinel@gmail.com>
2025-02-01 21:38:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export default function Page() {
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
+      "ok ur talking to caty's ghost",
       "please babe",
       ":((((",
       "PRETTY PLEASE",
