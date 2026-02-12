232d6fac4e6c2486b25fc81856277763da69691b
emhill0213 <em.hill.0213@gmail.com>
2026-02-11 22:28:27 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ export default function Page() {
       "Are you sure?",
       "What if I asked really nicely?",
       "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
+      "With chocolate cake on top",
+      "What about a strawberry daiquiri",
+      "PLEASE BESTIE",
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
+      "ok ur talking to emily's ghost",
       "please babe",
       ":((((",
       "PRETTY PLEASE",
@@ -38,7 +38,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you bestieee!! ;))</div>
         </>
       ) : (
         <>
